4fb18bf3ac9a4d6cf21b0bbd5f0740607cc4fc89
Arutyunyan Artavazd <artwrkml@gmail.com>
2026-04-20 17:18:10 +0300

    fix localhost url issue

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -2,26 +2,20 @@ package hooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.BeforeAll;
 import org.openqa.selenium.chrome.ChromeOptions;
 import utils.ScenarioContext;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 
 public class Hooks {
 
     @BeforeAll
     public static void setUpAll() {
-        ChromeOptions options = new ChromeOptions();
         Configuration.browser = "chrome";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        Configuration.browserSize = null;
         Configuration.browserCapabilities = options;
-    }
-
-    @Before
-    public void setUp() {
-        Selenide.open();
-        WebDriverRunner.getWebDriver().manage().window().maximize();
     }
 
     @After
