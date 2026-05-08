74cfe39bc163c5aa51d89326d803c62972ca415a
Arutyunyan Artavazd <artwrkml@gmail.com>
2026-05-08 23:04:31 +0300

    final fixes

--- src/main/java/api/Specifications.java
+++ src/main/java/api/Specifications.java
@@ -3,7 +3,6 @@ package api;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
@@ -15,14 +14,12 @@ public class Specifications {
         return new RequestSpecBuilder()
                 .setBaseUri(url)
                 .setContentType(ContentType.JSON)
-                //.log(LogDetail.BODY)
                 .build();
     }
 
     public static ResponseSpecification baseResponseSpec()
     {
         return new ResponseSpecBuilder()
-                //.log(LogDetail.BODY)
                 .build();
     }
 }
